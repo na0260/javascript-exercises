f6f19636889b54dad2b0043a7e7631f1a75e6a71
na0260 <samjr.008@gmail.com>
2024-09-10 02:40:44 +0600

    factorial problem solved

---
+++ js-basics/exercise1.js
@@ -0,0 +1,10 @@
+// Write a function that calculates the factorial of a number.
+
+function fact(n){
+    if (n === 0 || n === 1)
+        return 1;
+    else
+        return n * fact(n-1);
+}
+
+console.log(fact(5));
