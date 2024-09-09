28c4df92b105f4c6bb386b1426153abb7e8e69c4
na0260 <samjr.008@gmail.com>
2024-09-10 03:33:11 +0600

    Dom exercise complete

---
+++ js-dom/main.js
@@ -0,0 +1,8 @@
+function changeBgColor() {
+  document.body.style.backgroundColor = 'lightblue';
+}
+
+function toggleVisibility() {
+  var text = document.getElementById('text');
+  text.style.display = text.style.display === 'none' ? 'block' : 'none';
+}
